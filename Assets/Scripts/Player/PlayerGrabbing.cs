af20aa87448bd7913df6d1e5f166241e240cb436
DESKTOP-2R1FIBP\jjohn <jjohnstone2520@gmail.com>
2018-08-04 18:50:40 +1200

    Added grabbing and dropping
    Added being able to grab pickupable objects and drop them

---
+++ Assets/Scripts/Player/PlayerGrabbing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabbing : MonoBehaviour {
+
+    public float grabDistance;
+    public float throwForce;
+
+    private bool isGrabbing = false;
+    private RaycastHit hit;
+    private Ray grabRay;
+
+    private void Update()
+    {
+        grabRay = new Ray(transform.position, transform.forward);
+
+        Debug.DrawRay(transform.position, transform.forward * grabDistance);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!isGrabbing)
+            {
+                if (Physics.Raycast(grabRay, out hit, grabDistance))
+                {
+                    if (hit.collider.tag == "Pickupable")
+                    {
+                        isGrabbing = true;
+                        hit.collider.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                        hit.collider.gameObject.transform.SetParent(transform);
+                        hit.collider.enabled = false;
+                    }
+                }
+            }
+            else
+            {
+                isGrabbing = false;
+                transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = false;
+                transform.GetChild(0).GetComponent<Collider>().enabled = true;
+                transform.GetChild(0).GetComponent<Rigidbody>().AddForce(transform.forward * throwForce);
+                transform.GetChild(0).SetParent(null);
+            }
+        }
+    }
+}
