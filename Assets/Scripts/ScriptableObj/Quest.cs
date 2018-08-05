0f900cef1abce593b26ed3a205f15fb3b2502f00
Enderderder <richardwulansari@gmail.com>
2018-08-05 22:26:14 +1200

    ~Started Making Quest Thing

---
+++ Assets/Scripts/ScriptableObj/Quest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : ScriptableObject
+{
+
+    public string questName;
+    public int questID;
+    public string discription;
+    public GameObject questGiver;
+
+
+}
