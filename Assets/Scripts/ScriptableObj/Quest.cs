073092912b164fd7777b4bccfa398dffaa269228
Enderderder <richardwulansari@gmail.com>
2018-08-06 02:03:24 +1200

    ..

--- Assets/Scripts/ScriptableObj/Quest.cs
+++ Assets/Scripts/ScriptableObj/Quest.cs
@@ -1,14 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+[CreateAssetMenu(fileName = "new Quest", menuName = "Quest")]
 public class Quest : ScriptableObject
 {
+    public UnityEvent OnQuestStart;
+    
 
     public string questName;
     public int questID;
     public string discription;
     public GameObject questGiver;
+
+
+    private void Awake()
+    {
+        if(OnQuestStart == null)
+        {
+            OnQuestStart = new UnityEvent();
+        }
+    }
+
+    public void QuestStart()
+    {
+        OnQuestStart.Invoke();
+    }
+    
 
 
 }
