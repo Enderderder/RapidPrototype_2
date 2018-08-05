94b7ded286914baa440c066416026b5352e1cfc0
Enderderder <richardwulansari@gmail.com>
2018-08-05 22:26:14 +1200

    ~Started Making Quest Thing

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    // Singleton Instance
+    public static QuestManager instance;
+
+    // Quest Lists
+    public List<Quest> possibleQuestList;
+    public List<Quest> currentQuestList;
+
+	void Awake ()
+    {  
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+	}
+
+
+}
