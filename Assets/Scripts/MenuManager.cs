97a2e2424cb36e292db6c3e26eaa329656462542
James81919 <jjohnstone2520@gmail.com>
2018-08-06 21:52:39 +1200

    Added how-to-play screen
    Added button to menu which takes you to the How-To-Play screen. Also a quit game question box will popup to confirm that you want to quit the game.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,13 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
 
-	public void Button_Play()
+    public Canvas menuCanvas;
+    public Canvas howToPlayCanvas;
+    public Canvas quitGameCanvas;
+
+    private void Awake()
+    {
+        menuCanvas.enabled = true;
+        howToPlayCanvas.enabled = false;
+        quitGameCanvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (howToPlayCanvas.enabled && Input.GetButtonDown("Cancel"))
+            Button_HowToPlay_Back();
+
+        if (quitGameCanvas.enabled && Input.GetButtonDown("Cancel"))
+            quitGameCanvas.enabled = false;
+    }
+
+    public void Button_Menu_Play()
     {
         SceneManager.LoadScene(1);
     }
 
-    public void Button_Quit()
+    public void Button_Menu_HowToPlay()
+    {
+        menuCanvas.enabled = false;
+        howToPlayCanvas.enabled = true;
+    }
+
+    public void Button_Menu_Quit()
+    {
+        quitGameCanvas.enabled = true;
+    }
+
+    public void Button_HowToPlay_Back()
+    {
+        menuCanvas.enabled = true;
+        howToPlayCanvas.enabled = false;
+    }
+
+    public void Button_Quit_Yes()
     {
         Application.Quit();
+    }
+
+    public void Button_Quit_No()
+    {
+        quitGameCanvas.enabled = false;
     }
 }
