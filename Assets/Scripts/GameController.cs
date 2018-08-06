af719038b2e59b9f50b28f546de5a3d28b6d99fb
James81919 <jjohnstone2520@gmail.com>
2018-08-06 20:10:23 +1200

    Added pause menu [WIP]
    Not working yet

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Canvas pauseMenuCanvas;
+
+    [System.NonSerialized] public bool isPaused = false;
+
+    private void Update()
+    {
+        if (isPaused)
+        {
+            pauseMenuCanvas.enabled = true;
+        }
+        else
+        {
+            pauseMenuCanvas.enabled = false;
+        }
+    }
+
+    public void Button_PauseMenu_Resume()
+    {
+        isPaused = false;
+    }
+
+    public void Button_PauseMenu_Quit()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
