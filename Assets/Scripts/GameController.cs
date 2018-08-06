4e938478c5bbe684b7df3e651f21ec271aef94db
Enderderder <richardwulansari@gmail.com>
2018-08-07 00:40:53 +1200

    ~ Still trying to make the quest system though.......

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
     public Canvas pauseMenuCanvas;
 
