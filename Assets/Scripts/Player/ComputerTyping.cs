f5495bc6a7822d87253da41568d61bbc93c6fb8a
RyouMisakl <33336762+RyouMisakl@users.noreply.github.com>
2018-08-07 18:07:58 +0800

    update

---
+++ Assets/Scripts/Player/ComputerTyping.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerTyping : MonoBehaviour {
+    public GameObject playerobject;
+
+
+    // Use this for initialization
+    void OnEnable () {
+        playerobject.GetComponent<PlayerController>().enabled = false;
+        playerobject.GetComponent<CameraController>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            playerobject.GetComponent<PlayerController>().enabled = true;
+            playerobject.GetComponent<CameraController>().enabled = true;
+            
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/KeyNote.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyNote : MonoBehaviour {
+    public GameObject closingnode;
+    public KeyCode key;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		closingnode.transform.localScale = new Vector3(closingnode.transform.localScale.x - 0.02f, closingnode.transform.localScale.y - 0.02f, closingnode.transform.localScale.z - 0.02f);
+        if (Input.GetKeyDown(key))
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (closingnode.transform.localScale.y <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
