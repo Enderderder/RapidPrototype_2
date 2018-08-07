3084cd7dfa99eb890273966bd84ee5d1c2dfb925
Enderderder <richardwulansari@gmail.com>
2018-08-07 16:49:24 +1200

    ~ Quest Progress

---
+++ Assets/Scripts/QuestSystem/Objective.cs
@@ -0,0 +1,23 @@
+﻿
+
+public class Objective
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool Compeleted { get; set; }
+    public int CurrentCount { get; set; }
+    public int TotalCount { get; set; }
+	
+    public virtual void Init()
+    {
+        // Base class Init
+    }
+
+    public void Evaluate()
+    {
+        if (CurrentCount >= TotalCount)
+        {
+            Compeleted = true;
+        }
+    }
+}
---
+++ Assets/Scripts/QuestSystem/Objectives/PickUpObjective.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObjective : Objective
+{
+    private GameObject PickUpObject { get; set; }
+
+
+
+    public PickUpObjective(Quest quest, GameObject pickUpObj, string description)
+    {
+        this.Description = description;
+        this.PickUpObject = pickUpObj;
+    }
+
+    public override void Init()
+    {
+        base.Init();
+
+
+    }
+
+}
