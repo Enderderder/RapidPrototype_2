d64e88daba22ae05adfc0710b2bdd330dfcfac9c
Enderderder <richardwulansari@gmail.com>
2018-08-08 17:56:21 +1200

    ~

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Interactable : MonoBehaviour
+{
+    public string ButtonToInteract { get; set; } = "E";
+    public Color OutlineColor { get; set; } = Color.yellow;
+    public bool AbleToPickUp { get; set; } = false;
+
+    private MeshRenderer Renderer { get; set; }
+
+    protected void Awake()
+    { 
+        Renderer = GetComponent<MeshRenderer>();
+    }
+
+
+    public void OutlineOn()
+    {
+        Renderer.material.shader = Resources.Load(
+            "Materials/ItemOutline", typeof(Shader)) as Shader;
+    }
+
+    public void ShowInteractButton()
+    {
+
+    }
+
+    public virtual void InteractAction()
+    {
+        // Do some interaction code
+    }
+}
---
+++ Assets/Scripts/Pickupable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : Interactable
+{
+    private new void Awake()
+    {
+        base.Awake();
+
+        this.AbleToPickUp = true;
+        this.ButtonToInteract = "Fire1";
+    }
+
+}
