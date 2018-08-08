3266327485c4b271b0953d34c234755150ce8c09
Enderderder <richardwulansari@gmail.com>
2018-08-09 02:32:30 +1200

    ~ Copy the level and edited it ~ Shader "ItemOutline" change

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -8,12 +8,16 @@ public class Interactable : MonoBehaviour
     public string ButtonToInteract { get; set; } = "E";
     public Color OutlineColor { get; set; } = Color.yellow;
     public bool AbleToPickUp { get; set; } = false;
-
     private MeshRenderer Renderer { get; set; }
+    private Material OriginMaterial { get; set; }
+    private Material OutlineMaterial { get; set; }
 
     protected void Awake()
     { 
         Renderer = GetComponent<MeshRenderer>();
+        OriginMaterial = Renderer.material;
+        OutlineMaterial = (Material)Resources.Load("Materials/ItemOutline", 
+            typeof(Material));
     }
 
 
@@ -21,6 +25,14 @@ public class Interactable : MonoBehaviour
     {
         Renderer.material.shader = Resources.Load(
             "Materials/ItemOutline", typeof(Shader)) as Shader;
+
+        // Change the Thickness and the Color of the outline shader
+        // base on the property
+    }
+
+    public void OutlineOff()
+    {
+        Renderer.material = OriginMaterial;
     }
 
     public void ShowInteractButton()
