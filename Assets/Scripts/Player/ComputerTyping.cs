b208351972186386ee62ec8b81467b7a6c7fb2d6
RyouMisakl <33336762+RyouMisakl@users.noreply.github.com>
2018-08-10 05:01:00 +0800

    .

--- Assets/Scripts/Player/ComputerTyping.cs
+++ Assets/Scripts/Player/ComputerTyping.cs
@@ -5,26 +5,11 @@ using UnityEngine;
 public class ComputerTyping : MonoBehaviour {
     public GameObject playerobject;
 
-
-    // Use this for initialization
-    void OnEnable () {
+    void OnEnable()
+    {
         playerobject.GetComponent<PlayerController>().enabled = false;
         playerobject.GetComponent<CameraController>().enabled = false;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            playerobject.GetComponent<PlayerController>().enabled = true;
-            playerobject.GetComponent<CameraController>().enabled = true;
-            
-            this.gameObject.SetActive(false);
-            //gameObject.transform.position = 
-        }
     }
 }
